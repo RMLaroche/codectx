bb981188114d077ec84f2bd03792080c232fb732
Remy Laroche <remy.laroche.33@gmail.com>
2025-07-24 09:44:53 +0200

    chore: bump version to 2.2.0

--- codectx/__init__.py
+++ codectx/__init__.py
@@ -2,6 +2,6 @@
 codectx - AI-powered code context and file summarization tool
 """
 
-__version__ = "2.1.0"
+__version__ = "2.2.0"
 __author__ = "RMLaroche"
 __email__ = "rmlaroche@users.noreply.github.com"
